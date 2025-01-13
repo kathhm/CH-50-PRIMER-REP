ccbb2c4c2784154e41deaf8b08494a459fac3ec0
Katherine Hinojosa <geofisikatt@gmail.com>
2025-01-13 14:39:05 -0600

    cambios que yo hice jeje

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function solicitarYCambiarNombre (){
  const nombre = prompt("Ingresa tu nombre");
  /*uso de la api DOM para cambiar el texto de un elemento*/
- document.getElementById("texto-saludo").innerText = "Hola " + nombre;
+ document.getElementById("texto-saludo").innerText = "Hola mi precioso " + nombre + " te amo con todo mi corazoncito, eres el mejor";
 
 }
